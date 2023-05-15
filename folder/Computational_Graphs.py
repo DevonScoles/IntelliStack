0ef9042c5bb51ae97cbb281cc772cb171243324a
Devon <Scolesdevon@gmail.com>
2023-05-15 18:43:55 -0400

    Optimization and Training

---
+++ folder/Computational_Graphs.py
@@ -0,0 +1,57 @@
+#Computation Graphs and Automatic Differentiation
+import torch
+import numpy as np
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim 
+
+# VARIABLES AND BACKPROPAGATION FUNCITON
+"""
+#Variables wrap tensor objects
+x = autograd.Variable( torch.Tensor([1,2,3]), requires_grad=True)
+#to access data use .data
+# print(x.data)
+
+#Variables can do same operations as Tensors
+y = autograd.Variable( torch.Tensor([4,5,6]), requires_grad=True)
+z = x + y
+# print(z)
+
+# print(z.grad_fn
+
+s = z.sum()
+# print(s,z)
+# print(s.grad_fn)
+# derrivative of sum with respect to first component of x:
+# ds / dx_0
+# s knows it was created from the sum of z and z knows it was created from the sum of x + y so:
+# s = z_0{x_0 + y_0} + z_1{x_1 + y_1} + z_2{x_2 + y_2}
+
+
+# using .backward() on any variable will run backpropagation starting from it
+s.backward()
+print(x.grad)
+"""
+
+# WHAT NOT TO DO IN ORDER TO COMPUTER BACKPROP
+
+#understanding the following is pertinent to understanding deep learning
+x = torch.randn((2,2))
+y = torch.randn((2,2))
+z = x + y # because these are random Tensor types, backprop is not possible
+
+var_x = autograd.Variable(x)
+var_y = autograd.Variable(y)
+var_z = var_x + var_y
+#var_z contains enough info to compute gradients
+print("var_z.grad_fn", var_z.grad_fn)
+var_z_data = var_z.data #gets wrapped Tensor object out of var_z
+new_var_z = autograd.Variable( var_z_data ) #rewrap Tensor in new Variable
+
+# new Variable still DOES NOT have information to backprop to x and y
+print("new var_z.grad_fn",new_var_z.grad_fn)
+# because we broke the variable chain when we ripped the tensor out of var_z using .data
+# and created new_var_z we have broken the variable chain and 
+# the loss Variable we create wont be able to realize the Variable exists
+
---
+++ folder/DeepLearning_BuildingBlocks.py
@@ -0,0 +1,73 @@
+# DEEP LEARNING BUILDING BLOCKS: AFFINE MAPS, NON-LINEARITIES, and OBJECTIVES
+import torch
+import numpy as np
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim 
+
+# AFFINE MAPS
+"""
+#affine map is a function of f(x)
+# f(x) = Ax + b
+# from a matrix A and vectors x,b. b is refferenced to as the bias term
+
+# Pytorch and other deep learning frameworks 
+# map by row input instead of columns like linear algebra
+
+lin = nn.Linear(5, 3)
+data = autograd.Variable( torch.randn(2,5) ) # data is 2x5, A maps from 5 - 3
+# can we map "data" under A?
+
+print(lin(data)) #yes
+"""
+
+# NON-LINEARTIIES
+"""
+# with affine maps f(x) = Ax+b and g(x) = Cx+d what is f(g(x))?
+#   f(g(x)) = A(Cx+d)+b = AC+(Ad+b)
+#   AC is a matrix and Ad+b is a vector
+#   composing affine maps gives us an affine map
+#   using non-linearities between the affine layers we can make a much more powerful model
+
+data = autograd.Variable(torch.randn(2, 2))
+print(data, "\n")
+print(F.relu(data))
+"""
+
+# SOFTMAX AND PROBABILITIES
+"""
+#Softmax(x) is non-linearity which outputs probability distribution 
+# where each element is non-negative and sum over all components is 1
+#   exp(x_i) / Sigma_j (exp(x_j))
+#   where x is vector of numbers (any kind)
+
+# Softmax is also in torch.functional
+data = autograd.Variable(torch.randn(5))
+print(data)
+print(F.softmax(data, dim=0))
+print(F.softmax(data, dim=0).sum())
+print(F.log_softmax(data, dim=0))
+"""
+
+# Objective Functions(loss function or cost function)
+"""
+# loss function is calculated by choosing a training instance and running it through the NN
+#   then computing the loss of the output.
+#   The parameters of the model are updated via taking the derivative of the loss function
+#   confidently WRONG answers from the model will result in HIGH loss
+#   confidently RIGHT answers will result in LOW loss
+
+# loss functions like negative lof likelihood loss are common objective
+#   for multi-class classification.
+"""
+
+#OPTIMIZATION AND TRAINING
+"""
+# Variable's konw how to compute gradients with respect to the 
+# things (Tensors) that were used to compute it
+#since our loss is an autograd.Variable() we can compute the gradients
+
+# Using the vanilla Standard Gradient Descient algorithm is okay but using
+#  different algorithms for this such as Adam or RMSProp will boost performance
+"""
